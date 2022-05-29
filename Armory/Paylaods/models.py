0c1679eadba9ce0deb8ee01e8ca500751d86b85f
chris <cahoppa1@gmail.com>
2022-05-29 15:54:45 -0400

    committiiiiiiboii

--- Armory/Paylaods/models.py
+++ Armory/Paylaods/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class ReturnedCipher(models.Model):
+    retuend = CharField(max_value=245, default='')
+
+class Seed1(models.Model):
+    seed_1 = models.IntegerField(default=1)
+    seed_2 = models.IntegerField(default=1)
+    inherited = models.ForeignKey (
+        ReturnedCipher, on_delete=models.CASCADE
+    )
+    
+class Response(models.Model):
+    cipher = models.CharField(default=None)
+    depth = models.IntegerField(default=0)
+    
+    
